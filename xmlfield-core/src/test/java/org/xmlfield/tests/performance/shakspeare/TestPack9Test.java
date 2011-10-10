19cd54091ee107be5d2e3f1b7c938d914500a065
Olivier Lafon <olivier.lafon@gmail.com>
2011-10-10 09:36:40 +0000

    Renommage des tests

--- xmlfield-core/src/test/java/org/xmlfield/tests/performance/shakspeare/TestPack9Test.java
+++ xmlfield-core/src/test/java/org/xmlfield/tests/performance/shakspeare/TestPack9Test.java
@@ -26,6 +26,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xmlfield.core.XmlFieldBinder;
 import org.xmlfield.core.impl.DefaultXmlFieldSelectorTest;
