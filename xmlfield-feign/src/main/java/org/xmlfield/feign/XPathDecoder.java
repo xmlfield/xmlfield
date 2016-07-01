bf6ea0674acea886b2091a013325c88254046354
Idriss Neumann <idriss.neumann@docpost.fr>
2016-07-01 18:17:47 +0200

    Adding feign decoding/encoding support

---
+++ xmlfield-feign/src/main/java/org/xmlfield/feign/XPathDecoder.java
@@ -0,0 +1,48 @@
+package org.xmlfield.feign;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import org.xmlfield.core.XmlField;
+import org.xmlfield.core.exception.XmlFieldParsingException;
+
+import feign.FeignException;
+import feign.Response;
+import feign.Util;
+import feign.codec.DecodeException;
+import feign.codec.Decoder;
+
+/**
+ * Decoder which can be used to unmarshall with XMLField.<br />
+ * Inspired by the JaxbDecoder from feign :
+ * https://github.com/Netflix/feign/blob/master/jaxb/src/main/java/feign/jaxb/JAXBDecoder.java
+ * 
+ * @author Idriss Neumann<neumann.idriss@gmail.com>
+ *
+ */
+public class XPathDecoder implements Decoder {
+  private XmlField xf = new XmlField();
+
+  @Override
+  public Object decode(Response response, Type type)
+      throws IOException, DecodeException, FeignException {
+
+    if (response.status() == 404) {
+      return Util.emptyValueOf(type);
+    }
+
+    if (response.body() == null) {
+      return null;
+    }
+
+    try {
+      return xf.xmlToObject(response.body().toString(), (Class<?>) type);
+    } catch (XmlFieldParsingException e) {
+      throw new DecodeException(e.toString(), e);
+    } finally {
+      if (response.body() != null) {
+        response.body().close();
+      }
+    }
+  }
+}
---
+++ xmlfield-feign/src/main/java/org/xmlfield/feign/XPathEncoder.java
@@ -0,0 +1,32 @@
+package org.xmlfield.feign;
+
+import java.lang.reflect.Type;
+
+import org.xmlfield.core.XmlField;
+import org.xmlfield.core.exception.XmlFieldParsingException;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
+
+/**
+ * Encoder which can be used to marshall with XMLField. Inspired by the Feign JaxbEncoder :
+ * https://github.com/Netflix/feign/blob/master/jaxb/src/main/java/feign/jaxb/JAXBEncoder.java
+ * 
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
+ *
+ */
+public class XPathEncoder implements Encoder {
+  private XmlField xf = new XmlField();
+
+  @Override
+  public void encode(Object object, Type bodyType, RequestTemplate template)
+      throws EncodeException {
+
+    try {
+      template.body(xf.objectToXml(object));
+    } catch (XmlFieldParsingException e) {
+      throw new EncodeException(e.toString(), e);
+    }
+  }
+}
