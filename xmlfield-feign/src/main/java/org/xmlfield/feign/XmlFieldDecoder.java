e9df743af12af3e27dfb5315e97a546d60d303dc
Idriss Neumann <idriss.neumann@docpost.fr>
2016-07-07 19:00:11 +0200

    Encoding as constructor parameter

--- xmlfield-feign/src/main/java/org/xmlfield/feign/XmlFieldDecoder.java
+++ xmlfield-feign/src/main/java/org/xmlfield/feign/XmlFieldDecoder.java
@@ -21,6 +21,26 @@ import feign.codec.Decoder;
  *
  */
 public class XmlFieldDecoder implements Decoder {
+  private static final String DEFAULT_ENCODING = "UTF-8";
+
+  private String encoding;
+
+  /**
+   * Instanciate the decoder with the specified encoding.
+   * 
+   * @param encoding
+   */
+  public XmlFieldDecoder(String encoding) {
+    this.encoding = encoding;
+  }
+
+  /**
+   * Instanciate the decoder with the default encoding (UTF-8).
+   */
+  public XmlFieldDecoder() {
+    this(DEFAULT_ENCODING);
+  }
+
   /**
    * {@inheritDoc}
    */
@@ -37,8 +57,8 @@ public class XmlFieldDecoder implements Decoder {
     }
 
     try {
-      return XmlFieldFactory.getInstance()
-          .xmlToObject(IOUtils.toString(response.body().asInputStream(), "UTF-8"), (Class<?>) type);
+      return XmlFieldFactory.getInstance().xmlToObject(
+          IOUtils.toString(response.body().asInputStream(), encoding), (Class<?>) type);
     } catch (XmlFieldParsingException e) {
       throw new DecodeException(e.toString(), e);
     } finally {
